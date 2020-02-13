a8e119ee14ecb7685979477fe436b856d09d6a77
asadikhlas <asadikhlas03@gmail.com>
2020-02-13 10:12:03 +0500

    13th Feb

--- 11thFeb/todayTask.js
+++ 11thFeb/todayTask.js
@@ -1,7 +1,14 @@
-function myFunc(a, b){return  }
+
+function myFunc(a, b){return [...new Set([...a, ...b])].sort(function(a, b){return a - b})}
 console.log(myFunc([1, 2, 50, 25, 5, 90], [6, 90, 2, 1 , 88, 100]))
+
 //result [1, 2, 5, 6, 25, 50, 88, 90, 100]
+
 
 //remove duplications
 //merge both arrays
 //sorting
+
+// console.log(a.sort(function(a,b) { return a - b; }))
+
+
